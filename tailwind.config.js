1dc6940535561888a1b169a16c9a96d6f2301a47
Hyeonjong <nianelo4@gmail.com>
2024-07-17 23:49:23 +0900

    Extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 const plugin = require('tailwindcss/plugin');
+const {
+  tjClassNames,
+  tjTheme,
+  tjPlugin,
+} = require('tailwind-joy/tw-extension');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', { raw: tjClassNames }],
   theme: {
     extend: {
       fontFamily: {
         sans: ['var(--font-pretendard)', ...fontFamily.sans],
       },
+      colors: tjTheme.colors,
+      keyframes: tjTheme.keyframes,
+      animation: tjTheme.animation,
     },
   },
   plugins: [
@@ -18,5 +26,6 @@ module.exports = {
         '@media (hover: hover) and (pointer: fine)',
       );
     }),
+    tjPlugin,
   ],
 };
